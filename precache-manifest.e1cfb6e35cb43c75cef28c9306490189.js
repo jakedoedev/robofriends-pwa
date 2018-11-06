21d51da8a0987035d7002bb55246b46223e264ab
Martin Louie Dela Serna <mldelaserna@Martins-MacBook-Pro.local>
2018-11-06 22:51:14 +0800

    Updates

---
+++ precache-manifest.e1cfb6e35cb43c75cef28c9306490189.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends-pwa/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "2581e180eec6a465185f",
+    "url": "/robofriends-pwa/static/js/runtime~main.2581e180.js"
+  },
+  {
+    "revision": "85c0b4ee84c40f3f9548",
+    "url": "/robofriends-pwa/static/js/main.85c0b4ee.chunk.js"
+  },
+  {
+    "revision": "6f34aa39559cde937802",
+    "url": "/robofriends-pwa/static/js/1.6f34aa39.chunk.js"
+  },
+  {
+    "revision": "85c0b4ee84c40f3f9548",
+    "url": "/robofriends-pwa/static/css/main.30c7371a.chunk.css"
+  },
+  {
+    "revision": "6f34aa39559cde937802",
+    "url": "/robofriends-pwa/static/css/1.66534053.chunk.css"
+  },
+  {
+    "revision": "ede28f5112c9c23caf40011ea0a14539",
+    "url": "/robofriends-pwa/index.html"
+  }
+];
