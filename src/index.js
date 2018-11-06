2daf25dd7884eb219f9608cb2b2ee55117e2911b
Martin Louie Dela Serna <mldelaserna@Martins-MacBook-Pro.local>
2018-11-06 18:22:46 +0800

    Add gh-pages plugin and remove logger in index

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, combineReducers} from 'redux';
-import { createLogger } from 'redux-logger';
+// import { createLogger } from 'redux-logger';
 import thunkMiddleware from 'redux-thunk';
 import './index.css';
 import App from './containers/App';
@@ -10,9 +10,10 @@ import * as serviceWorker from './serviceWorker';
 import 'tachyons';
 import { searchRobots, requestRobots } from './reducers';
 
-const logger = createLogger();
+// const logger = createLogger();
 const rootReducer = combineReducers({searchRobots, requestRobots});
-const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, logger));
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
+// const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, logger));
 
 ReactDOM.render(
   <Provider store={store}>
@@ -24,4 +25,5 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
+serviceWorker.register();
